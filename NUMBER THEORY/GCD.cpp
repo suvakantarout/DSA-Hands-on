026f568237786036b5d08a130522bdcd0e887f70
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-01-31 15:37:15 +0530

    31.01.2025

---
+++ NUMBER THEORY/GCD.cpp
@@ -0,0 +1,33 @@
+//find GCD -> Greatest common divisor of a number ?
+
+#include<iostream>
+using namespace std;
+
+int GCD(int m, int n){
+	int GCD = 1;
+	int min;
+	if(m < n){
+		min = m;
+	}
+	else{
+		min = n;
+	}
+	for(int i = 1; i<=min; i++){
+		if(m % i == 0 && n % i == 0){
+			GCD = i;
+		}
+	}
+	return GCD;
+	
+}
+
+int main(){
+	int m,n;
+	cout<<"Enter two number: "<<endl;
+	cin>>m>>n;
+	
+	int ans = GCD(m,n);
+	cout<<"Greatest common divisor = "<<ans;
+	
+	return 0;
+}
