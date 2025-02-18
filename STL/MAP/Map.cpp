d37edcd19367d9a82443b66112c25ba264b2513f
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-02-18 21:48:53 +0530

    18-0202025

--- STL/MAP/Map.cpp
+++ STL/MAP/Map.cpp
@@ -4,24 +4,40 @@
 using namespace std;
 
 int main(){
+	
 	unordered_map<string, string> table;
 	
-	table["in"] = "india";
-	table.insert(make_pair("en", "England"));
-	table.insert(make_pair("in", "India"));
-	table.insert(make_pair("h", "Hongkong"));
+//	Insertion
+	table["in"] = "india";     // Process-1
+	
+	pair<string, string>p      // Process-2
+	p.first = "en";
+	p.second = "England";
+	table.insert(p);
+	pair<string, string>q;
+	q.first = "h";
+	q.second = "Hongkong";
+	table.insert(q);
 	
 	cout<<table.size()<<endl;
 	
 //	table.clear();
 	
 	cout<<table.at("in")<<endl;
-	
-	unordered_map<string, string>:: iterator it;
-	for(int it = table.begin(); it<= table.end(); it++){
-		cout<<*it<<endl;
+
+
+//	Traversing through a map
+
+//	unordered_map<string, string>:: iterator it;
+//	for(int it = table.begin(); it<= table.end(); it++){
+//		cout<<*it<<endl;
+//	}
+	for(auto it : table){
+		cout<<it.first <<"->" <<it.second<<endl;
 	}
 	
+	
+//	Finding key
 	if(table.find("in") != table.end()){
 		cout<<"Key found"<<endl;
 	}
@@ -30,8 +46,9 @@ int main(){
 		cout<<"Key not found";
 	}
 	
+	
+//	counting the key
 //	table.count();
-//	
 	if(table.count("in") == 0){
 		cout<<"Key not found"<<endl;
 	}
---
+++ STL/SET/set.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main(){
+	
+//	unordered_set<int> s;  ==> print elements in random order
+	set<int> st;
+	
+	st.insert(10);
+	st.insert(15);
+	st.insert(8);
+	st.insert(4);
+	st.insert(100);
+	
+	//traversal
+	for(auto it : st){
+		cout<<it<<" ";	
+	}cout<<endl;
+	
+	// Get size
+	cout<<st.size() <<endl;
+	// clear the set
+	st.clear();
+	cout<<st.size()<<endl;
+	
+	// check empty or not
+	st.empty() ? cout<<"SET is empty": cout<<"Not empty";
+
+	//earse elements  from the set
+	st.erase(st.begin(),st.end());
+	cout<<endl;
+	cout<<st.size()<<endl;
+	
+	
+	return 0;
+}
