bf0be49cbb7b9a9d2ab875aa5aa6ac8e2a7a0630
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-03-11 23:09:49 +0530

    String

--- STRING/String.cpp
+++ STRING/String.cpp
@@ -1,26 +1,16 @@
 #include<iostream>
 using namespace std;
- 
+
 int main(){
-	
-//	Creating a string
-	string str;
-	
-//	str.push_back('S');
-//	str.push_back('U');
-//	str.push_back('N');
-//	str.push_back('I');
-//	str.push_back('T');
-//	str.pop_back();
-//	cout<<"Enter the input"<<endl;
-//	getline(cin, str,'\n' );
-//	cout<<"String is: "<<str<<endl;
-	
-	auto it;
-	for(it = str.begin(); it < str.end(); it++){
-		cout<< *it<<endl;
-	}
-	
-	
-	return 0;
-} 
+    // string creation
+    char str[100];
+
+    // string input
+    cout<<"Enter your name: "<<endl;
+    cin>>str;
+
+    // print the string
+    cout<<"Your name is: "<<str<<endl;
+
+    return 0;
+}
---
+++ STRING/String_STL.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+ 
+int main(){
+	
+//	Creating a string
+	string str;
+	
+//	str.push_back('S');
+//	str.push_back('U');
+//	str.push_back('N');
+//	str.push_back('I');
+//	str.push_back('T');
+//	str.pop_back();
+//	cout<<"Enter the input"<<endl;
+//	getline(cin, str,'\n' );
+//	cout<<"String is: "<<str<<endl;
+	
+	auto it;
+	for(it = str.begin(); it < str.end(); it++){
+		cout<< *it<<endl;
+	}
+	
+	
+	return 0;
+} 
