66dd887578c1e2940b778906d6f5bd03556654a2
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-03-11 23:53:58 +0530

    added

--- STRING/String.cpp
+++ STRING/String.cpp
@@ -7,10 +7,15 @@ int main(){
 
     // string input
     cout<<"Enter your name: "<<endl;
-    cin>>str;
+    cin.getline(str,5);
 
     // print the string
     cout<<"Your name is: "<<str<<endl;
+    cout<<str[0] <<"-> " <<(int)str[0] <<endl;
+    cout<<str[1] <<"-> " <<(int)str[1] <<endl;
+    cout<<str[2] <<"-> " <<(int)str[2] <<endl;
+    cout<<str[3] <<"-> " <<(int)str[3] <<endl;
+    cout<<str[4] <<"->  " <<(int)str[4] <<endl;  //Null character
 
     return 0;
 }
