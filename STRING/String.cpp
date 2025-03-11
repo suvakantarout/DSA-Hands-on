7f8652068ec087c24b7257794af931de54f03c36
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-03-11 23:59:30 +0530

    Delimeter in string

--- STRING/String.cpp
+++ STRING/String.cpp
@@ -17,5 +17,11 @@ int main(){
     cout<<str[3] <<"-> " <<(int)str[3] <<endl;
     cout<<str[4] <<"->  " <<(int)str[4] <<endl;  //Null character
 
+    cout<<"Enter another string: ";
+    cin.getline(str, 100, '.');  //(string name, size, delimeter)
+    // Input will stop the delimeter found 
+    cout<< endl << str <<endl;
+
+
     return 0;
 }
