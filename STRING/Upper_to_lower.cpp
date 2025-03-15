646099128024a96346b68988ec57735829d1b098
Suvakanta Rout <113529234+suvakantarout@users.noreply.github.com>
2025-03-15 20:49:52 +0530

    .

---
+++ STRING/Upper_to_lower.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void ToUpper(char str[], int size){
+    for(int i = 0; i<size; i++){
+        if(str[i] >= 'a' && str[i] <= 'z'){
+            str[i] = (int)str[i] - 32;
+        }     
+    }
+}
+
+void ToLower(char str[], int size){
+    for(int i = 0; i<size; i++){
+        if(str[i] >= 'A' && str[i] <= 'Z'){
+            str[i] = (int)str[i] + 32;
+        }
+    }
+}
+
+
+int main(){
+    char str[100];
+    int size = 100;
+    cout<<"Enter a string to convert: ";
+    cin>>str;
+
+    ToUpper(str, size);
+    cout<<"Upper case string is: "<<str<<endl;
+
+    ToLower(str, size);
+    cout<<"Lower case string is: "<<str;
+
+    return 0;
+}
